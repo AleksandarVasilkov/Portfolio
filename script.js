72cb5898eab85689546466c770e3efa533a4ca58
Aleksandar Vasilkov <110114680+AleksandarVasilkov@users.noreply.github.com>
2024-01-24 23:20:40 +0000

    2nd submission

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var searchInput = document.getElementById('searchInput');
+
+    searchInput.addEventListener('input', function() {
+        var query = this.value.toLowerCase();
+        var projectCards = document.querySelectorAll('.main-container .project-card');
+
+        projectCards.forEach(function(card) {
+            var title = card.querySelector('h2').textContent.toLowerCase();
+            var description = card.querySelector('h3').textContent.toLowerCase();
+
+            if (title.includes(query) || description.includes(query)) {
+                card.style.display = '';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
+});
 
 
 
